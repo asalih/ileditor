07e0f69ad139efa633dcc62f55b2d71e2c88d35a
Ahmet Salih <ahmet.salih@windowslive.com>
2016-07-25 12:43:58 +0300

    iltest&sln

---
+++ iltest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iltest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataWait = Console.ReadLine();
+            WR(dataWait);
+
+            Console.ReadLine();
+        }
+
+        public static void WR(string data)
+        {
+            Console.WriteLine(data);
+        }
+
+        public void Payment()
+        {
+            
+            //process...
+        }
+    }
+}
