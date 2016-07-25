e79cda5f89ab726a6d1db2ba9a1b828b9b24bb34
Ahmet Salih <ahmet.salih@windowslive.com>
2016-07-25 12:43:29 +0300

    ileditor

---
+++ ileditor/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ileditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.txtILCode = new System.Windows.Forms.RichTextBox();
+            this.fileSelector = new System.Windows.Forms.OpenFileDialog();
+            this.btnFileList = new System.Windows.Forms.ToolStripDropDownButton();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnFileList});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(953, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // txtILCode
+            // 
+            this.txtILCode.BackColor = System.Drawing.SystemColors.Info;
+            this.txtILCode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtILCode.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtILCode.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtILCode.Location = new System.Drawing.Point(0, 25);
+            this.txtILCode.Name = "txtILCode";
+            this.txtILCode.Size = new System.Drawing.Size(953, 433);
+            this.txtILCode.TabIndex = 1;
+            this.txtILCode.Text = "";
+            // 
+            // fileSelector
+            // 
+            this.fileSelector.FileName = "*.*";
+            // 
+            // btnFileList
+            // 
+            this.btnFileList.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnFileList.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.btnFileList.Image = ((System.Drawing.Image)(resources.GetObject("btnFileList.Image")));
+            this.btnFileList.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnFileList.Name = "btnFileList";
+            this.btnFileList.Size = new System.Drawing.Size(38, 22);
+            this.btnFileList.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 458);
+            this.Controls.Add(this.txtILCode);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.RichTextBox txtILCode;
+        private System.Windows.Forms.OpenFileDialog fileSelector;
+        private System.Windows.Forms.ToolStripDropDownButton btnFileList;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
+
---
+++ ileditor/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ileditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public string IldasmPath { get { return @"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\ildasm.exe"; } }
+        public string IlasmPath { get { return @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\ilasm.exe"; } }
+
+        public string CurrentAsmPath { get; set; }
+
+        public string ParameterILASM { get; set; }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fileSelector.ShowDialog() == DialogResult.OK)
+            {
+                var path = fileSelector.FileName;
+
+                int dllIdx = path.LastIndexOf(".dll");
+
+                if(dllIdx > 0)
+                {
+                    ParameterILASM = "/dll";
+                    this.CurrentAsmPath = path.Substring(0, dllIdx) + ".asm";
+                }
+                else
+                {
+                    ParameterILASM = "/exe";
+                    this.CurrentAsmPath = path.Substring(0, path.LastIndexOf(".exe")) + ".asm";
+                }
+                
+
+
+                Process info = Process.Start(this.IldasmPath, string.Format("\"{0}\" /out={1}",  path, this.CurrentAsmPath));
+                info.WaitForExit();
+
+                txtILCode.Text = File.ReadAllText(this.CurrentAsmPath);
+
+
+
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(this.CurrentAsmPath, txtILCode.Text);
+
+            Process info = Process.Start(this.IlasmPath, this.ParameterILASM + " " + this.CurrentAsmPath);
+            info.WaitForExit();
+
+            MessageBox.Show("Done!");
+        }
+    }
+}
